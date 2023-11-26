5e3c1da8b2c97d0c0ad655f354a3bbedb0b430f3
Luísa Gontijo <luisaporf@gmail.com>
2023-11-26 15:17:08 -0300

    fix(main): problemas no main

--- main.cpp
+++ main.cpp
@@ -20,142 +20,148 @@ int main() {
         cin >> comando;
 
       
-        if (comando.size() >= 2) { //divide a string e verifica
-            switch (comando[0]) {
+        if (comando.size() < 2) {
+            cout << "Comando inválido.\n";
+            return 0;
+        }
 
-                case 'C':
-                    switch (comando[1]) {
 
-                        case 'F': {
-                            cout << "CADASTRAR FILME" << endl;
+        switch (comando[0]) {
 
-                            cout << "Digite o tipo (D para DVD ou F para fita): " << endl;
-                            char tipo; 
-                            cin >> tipo;
-                            if (tipo != 'F' && tipo != 'D') {
+            case 'C': {
+                switch (comando[1]) {
+
+                    case 'F': {
+                        cout << "CADASTRAR FILME" << endl;
+
+                        cout << "Digite o tipo (D para DVD ou F para fita): " << endl;
+                        char tipo; 
+                        cin >> tipo;
+                        if (tipo != 'F' && tipo != 'D') {
+                            cout << "ERRO: dados incorretos\n";
+                            return;
+                        }
+
+                        cout << "Digite o número de unidades:" << endl;
+                        int unidades;
+                        cin >> unidades;
+
+                        cout << "Digite o código:" << endl;
+                        int id; 
+                        cin >> id;
+
+                        cout << "Digite o título:" << endl;
+                        string titulo; 
+                        getline(cin,titulo);
+
+                        char categoria = 'X';
+                        if (tipo == 'D') {
+                            cout << "Digite o categoria do DVD (E, L ou P):" << endl;
+                            cin >> categoria;
+                            if (categoria != 'E' && categoria != 'L' && categoria != 'P') {
                                 cout << "ERRO: dados incorretos\n";
                                 return;
                             }
-
-                            cout << "Digite o número de unidades:" << endl;
-                            int unidades;
-                            cin >> unidades;
-
-                            cout << "Digite o código:" << endl;
-                            int id; 
-                            cin >> id;
-
-                            cout << "Digite o título:" << endl;
-                            string titulo; 
-                            getline(cin,titulo);
-
-                            char categoria = 'X';
-                            if (tipo == 'D') {
-                                cout << "Digite o categoria do DVD (E, L ou P):" << endl;
-                                cin >> categoria;
-                                if (categoria != 'E' && categoria != 'L' && categoria != 'P') {
-                                    cout << "ERRO: dados incorretos\n";
-                                    return;
-                                }
-                            }
-
-                            CineMax.cadastrarFilme(tipo, unidades, id, titulo, categoria);
-                            break;
                         }
 
-                        case 'C': // cadastrar cliente   
-                           
-                            break;
-                    
-                        default:
-                            cout << "Comando inválido.\n";
-                            break;
+                        CineMax.cadastrarFilme(tipo, unidades, id, titulo, categoria);
+                        break;
                     }
-                    break;
-                case 'R':
-                    switch (comando[1]) {
-                        case 'F':
-                            // remover filme  
-                           
-                            break;
 
-                        case 'C':
-                            // remover cliente
-                           
-                            break;
-                                                                                       //outros casos?
-                        default:
-                           
-                            break;
-                    }
-                    break;
-               
-                case 'L':
-                    switch (comando[1]) {
-                        case 'F':
-                            // lista  filmes
-                            
-                           
-                            break;
-
-                        case 'C':
-                            // lista clientes
-                          
-                            break;
+                    case 'C': // cadastrar cliente   
                         
-                        default:
-                            cout << "Comando inválido.\n";
-                            break;
-                    }
-                    break;
+                        break;
                 
-                case 'A':
-                    switch (comando[1]) {
-                        case 'L':
-                            
-                            // aluguel
-                            
-                            break;
-                        
-                        default:
-                            cout << "Comando inválido.\n";
-                            break;
-                    }
-                    break;
-                case 'D':
-                    switch (comando[1]) {
-                        case 'V':    
-                            // devolucao
-                            break;
-
-                        default:
-                            cout << "Comando inválido.\n";
-                            break;
-                    }
-                    break;
-                case 'F':
-                    switch (comando[1]) {
-                        case 'S':
-
-                            cout << "Programa encerrado!\n";
-                            return 0;
-
-                        default:
-                            cout << "Comando inválido.\n";
-                            break;
-                    }
-                    break;
-
-                default:
-                    cout << "Comando inválido.\n";
-                    break;
+                    default:
+                        cout << "Comando inválido.\n";
+                        break;
+                }
+                break;
             }
+            case 'R': {
+                switch (comando[1]) {
+                    case 'F':
+                        // remover filme  
+                        
+                        break;
 
-        } 
-        
-        else {
-            cout << "Comando inválido \n";
+                    case 'C':
+                        // remover cliente
+                        
+                        break;
+                                                                                    //outros casos?
+                    default:
+                        
+                        break;
+                }
+                break;
+            
+            case 'L':
+                switch (comando[1]) {
+                    case 'F':
+                        // lista  filmes
+                        
+                        
+                        break;
+
+                    case 'C':
+                        // lista clientes
+                        
+                        break;
+                    
+                    default:
+                        cout << "Comando inválido.\n";
+                        break;
+                }
+                break;
+            }
+            case 'A': {
+                switch (comando[1]) {
+                    case 'L':
+                        
+                        // aluguel
+                        
+                        break;
+                    
+                    default:
+                        cout << "Comando inválido.\n";
+                        break;
+                }
+                break;
+            }
+            case 'D': {
+                switch (comando[1]) {
+                    case 'V':    
+                        // devolucao
+                        break;
+
+                    default:
+                        cout << "Comando inválido.\n";
+                        break;
+                }
+                break;
+            }
+            case 'F': {
+                switch (comando[1]) {
+                    case 'S':
+
+                        cout << "Programa encerrado!\n";
+                        return 0;
+
+                    default:
+                        cout << "Comando inválido.\n";
+                        break;
+                }
+                break;
+            }
+            default: {
+                cout << "Comando inválido.\n";
+                break;
+            }
         }
+        
     }
-    return 0;
+
+    return 0;
+
 }
