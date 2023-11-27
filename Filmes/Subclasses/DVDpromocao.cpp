c6ea93dc99790e13f37adfbca43e20e991c1e215
warley004 <warleyjunio2004@gmail.com>
2023-11-27 19:56:45 -0300

    doc DVDpromocao.cpp

--- Filmes/Subclasses/DVDpromocao.cpp
+++ Filmes/Subclasses/DVDpromocao.cpp
@@ -15,9 +15,9 @@ dvdPromocao::dvdPromocao(int id, string titulo, int unidades) : Filme(id, titulo
 /**
  * @brief Método "calcularValor" da classe "dvdPromocao", utilizado especialmente para controle e registro.
  * @details Esse método calcula quanto o cliente terá de pagar pelo aluguel do filme de acordo com a quantidade de dias
- * que ele ficou alugado.
+ * que ele ficou alugado. Como o DVD está é da categoria "promoção", seu valor é fixo em 10 reais.
  * @param dias Armazena quantos dias um determinado filme ficou alugado.
- * @return Retorna o preço que o cliente terá de pagar pelo aluguel do filme, sendo 10 reais por dia o preço da locação
+ * @return Retorna o preço que o cliente terá de pagar pelo aluguel do filme, sendo 10 reais fixos o preço da locação
  * de um DVD da categoria "Promoção".
 */
 
