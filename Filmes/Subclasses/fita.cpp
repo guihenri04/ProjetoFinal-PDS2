0d11afd160e83f21ff7aa576e96a14aaa17ff103
warley004 <warleyjunio2004@gmail.com>
2023-11-27 20:55:29 -0300

    documentação fita.cpp e fita.hpp

--- Filmes/Subclasses/fita.cpp
+++ Filmes/Subclasses/fita.cpp
@@ -1,13 +1,38 @@
 #include "fita.hpp"
 
+/**
+ * @file fita.cpp
+ * @brief Construtor do arquivo "fita.hpp", para identificação e controle dos filmes da locadora.
+ * @details Esse construtor inclui uma chamada para o construtor da classe base ("Filme"), o qual contém os mesmos parâmetros para
+ * identificação da fita ("(int id, string titulo, int unidades)"). Além disso, define o valor de "tipo" como "FITA" e o valor de 
+ * "rebobinada" como "true".
+ * @param tipo Armazena o tipo do filme (no caso, filme).
+ * @param rebobinada Determina se a fita foi rebobinada ou não.
+*/
+
 Fita::Fita(int id, string titulo, int unidades) : Filme(id, titulo, unidades) {
     this -> tipo = "FITA";
     this -> rebobinada = true;
 }
 
+/**
+ * @brief Método "rebobinar" da classe "Filme", utilizado especialmente para controle.
+ * @details Esse método determina que uma determinada fita foi rebobinada.
+*/
+
 void Fita::rebobinar() {
     this -> rebobinada = true;
 }
+
+/**
+ * @brief Método "calcularValor" da classe "Fita", utilizado especialmente para controle e registro.
+ * @details Esse método calcula quanto o cliente terá de pagar pelo aluguel da fita de acordo com a quantidade de dias
+ * que ela ficou alugada. Caso o cliente devolva a fita já rebobinada, o aluguel é fixo em 5 reais, caso contrário, é fixo em 7
+ * reais.
+ * @param dias Armazena quantos dias um determinado filme (no caso, fita) ficou alugado.
+ * @return Retorna o preço que o cliente terá de pagar pelo aluguel da fita, sendo 5 reais fixos o preço da locação
+ * de uma fita rebobinada e 7 reais fixos o de uma não rebobinada.
+*/
 
 int Fita::calcularValor(int dias) {
     if (this -> rebobinada) {
@@ -16,6 +41,12 @@ int Fita::calcularValor(int dias) {
         return 7;
     }
 }
+
+/**
+ * @brief Método "serAlugado" da classe "Filme", utilizado especialmente para controle.
+ * @details Esse método determina que, sempre que uma fita for alugada, ela será automaticamente considerada desrebobinada.
+*/
+
 void Fita::serAlugado() {
     this -> rebobinada = false;
 }
--- Filmes/Subclasses/fita.hpp
+++ Filmes/Subclasses/fita.hpp
@@ -10,18 +10,18 @@
  * código do arquivo "fita.cpp", especifica o tipo desses métodos (o que eles irão retornar) e os parâmetros que 
  * serão recebidos por eles.
  * @class Fita
- * @details Na classe "Fita", que herda publicamente a classe "Filme", existem como atributos privados
- * 
- * 
- * públicos um construtor, o qual
- * recebe como parâmetros o id de identificação de um filme, seu título e a quantidade de unidades disponíveis dele para locação
- * ("dvdEstoque(int id, string titulo, int unidades)"). Existe também um método para calcular quanto será pago pelo aluguel do filme
- * de acordo com a quantidade de dias que ele ficou alugado, o qual substitui uma função virtual na classe base, no caso, "Filme"
- * ("int calcularValor(int dias) override").
- * @param id Determina o id do filme para identificação.
- * @param titulo Armazena o nome do filme.
- * @param unidades Registra quantas unidades de um filme estão disponíveis para locação.
- * @param dias Registra quantos dias um determinado filme ficou alugado.
+ * @details Na classe "Fita", que herda publicamente a classe "Filme", existe como atributo privado a variável booleana "rebobinada",
+ * que determina se a fita foi rebobinada ou não. A classe também contém como atributos públicos um construtor, o qual
+ * recebe como parâmetros um id de identificação de uma fita, seu título e a quantidade de unidades dela disponíveis para locação
+ * ("Fita(int id, string titulo, int unidades)"), um método para determinar se a fita foi rebobinada ou não 
+ * ("void rebobinar()"), um método para calcular quanto será pago pelo aluguel da fita de acordo com a quantidade de dias que ela ficou 
+ * alugada ("("int calcularValor(int dias) override")") e um método para alugar as fitas disponíveis na locadora ("void serAlugado() override").
+ * Ambos métodos substituem funções virtuais na classe base, no caso,"Filme".
+ * @param rebobinada Determina se a fita foi rebobinada ou não.
+ * @param id Determina o id do filme (no caso, fita) para identificação.
+ * @param titulo Armazena o nome do filme (no caso, fita).
+ * @param unidades Registra quantas unidades de um filme (no caso, fita) estão disponíveis para locação.
+ * @param dias Registra quantos dias um determinado filme (no caso, fita) ficou alugado.
 */
 
 class Fita : public Filme {
