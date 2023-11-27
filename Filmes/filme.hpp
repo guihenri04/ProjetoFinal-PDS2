c8741577e2fba06abf8228ed464ede09444340a0
warley004 <warleyjunio2004@gmail.com>
2023-11-27 18:33:57 -0300

    documentação filme.hpp

--- Filmes/filme.hpp
+++ Filmes/filme.hpp
@@ -18,7 +18,7 @@ using namespace std;
  * id do filme ("int id"), a nota de avaliação recebida pelo filme ("float avaliação"), quantas vezes esse filme foi avaliado
  * ("int vezesAvaliado"), o nome do filme ("string titulo") e o tipo desse filme, seja fita, DVD, etc ("string tipo"). 
  * A classe também contém como atributos públicos um construtor que recebe o id, o nome e a quantidade disponível de determinado filme
- * ("Filme(int id, string titulo, int unidades)") e um destrutor ("~Filme()"). Existe também um método para calcular quanto será pago pelo
+ * ("Filme(int id, string titulo, int unidades)") e um destrutor virtual, inicializado como default ("~Filme()"). Existe também um método para calcular quanto será pago pelo
  * aluguel do filme, de acordo com o tempo de aluguel ("virtual int calcularValor(int dias) = 0"), um método para identificar
  * os filmes ("void lerFilme()"), um método para alugar os filmes disponíveis ("virtual void serAlugado()"), um método para devolver
  * os filmes que foram alugados ("void serDevolvido()") e um método para avaliar o filme alugado ("void serAvaliado(int nota)"). Além
