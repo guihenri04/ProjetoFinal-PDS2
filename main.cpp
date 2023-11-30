800b69590872a10836e4bf2b4a0ab65410459da8
guihenri04 <guihenri2004@hotmail.com>
2023-11-30 20:46:47 -0300

    feat: erros

--- main.cpp
+++ main.cpp
@@ -255,7 +255,7 @@ int main() {
 
                         else {
 
-                            printColoredText("ERRO: Arquivo inexistente!\n",31);
+                           printColoredText("ERRO: Arquivo inexistente!\n",31);
 
                         }
 
